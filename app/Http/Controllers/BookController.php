4594a2471f7f8020ab29c5a4ce842524979a9105
Ozodbek4252 <Ozodbekmcfly@gmail.com>
2023-02-09 16:10:32 +0500

    BookController path() and destroy

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,11 +9,20 @@ class BookController extends Controller
 {
     public function store(BookStoreRequest $request)
     {
-        Book::create($request->all());
+        $book = Book::create($request->all());
+
+        return redirect($book->path());
     }
 
     public function update(BookStoreRequest $request, Book $book)
     {
         $book->update($request->all());
+
+        return redirect($book->path());
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
     }
 }
