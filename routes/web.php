fc1243a62b4992c05dd4d41c72fed0333d16b312
Ozodbek4252 <Ozodbekmcfly@gmail.com>
2023-02-09 16:10:36 +0500

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/books', [BookController::class, 'store']);
 Route::patch('/books/{book}', [BookController::class, 'update']);
+Route::delete('/books/{book}', [BookController::class, 'destroy']);
+
+
+
+Route::post('/authors', [AuthorController::class, 'store']);
+
+
+
